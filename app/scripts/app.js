ab00e2ee8ed228672d3af24ff35d3875717844ef
Jérôme Moret <jermoret@gmail.com>
2016-01-20 20:44:57 +0100

    After yo generation

---
+++ app/scripts/app.js
@@ -0,0 +1,14 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name twebTe2App
+ * @description
+ * # twebTe2App
+ *
+ * Main module of the application.
+ */
+angular
+  .module('twebTe2App', [
+    'ngAnimate'
+  ]);
